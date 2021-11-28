bbfd3c6cba920273a4d0c43061b444823dfd9d72
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-27 20:12:18 -0500

    Adding a datamethod file that will determine user access based on provided info

---
+++ dataMethod.js
@@ -0,0 +1,31 @@
+const dataMethod = function (request, response, users) {
+  const userID = request.cookies["user_id"];
+  const email = request.cookies["authentic_email"];
+  const password = request.cookies["authentic_password"];
+
+
+/*Entered email and password is incorrect, the response reverts to an error.
+Otherwise, evaluates to true and allows user access to account*/
+
+  if (!pass && !email) {
+    for (user of users) {
+      if (request.body.input === user.email) {
+        response.cookie("user_id", user.id);
+        response.cookie("authentic_email", true);
+        response.send("email exists")
+        return;
+
+      }
+    }
+    response.status(403).send("The email entered is not registered");
+  }
+
+  /* Authentic email entered but incorrect password provided, reverts to error.
+  Otherwise returns as true and allows user access to account*/
+  else if (!pass && email) {
+    for (user of users) {
+
+    }
+  }
+}
+
