e9608527ca07bac14e9c811b015b56c13ea8fd81
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-29 21:57:11 -0500

    Added get and post routes for user id and favourites

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,10 @@
 
 const express = require('express');
 const router  = express.Router();
+const { addFavouriteMap, getFavouriteMaps} = require('../db/queries/users-queries');
 
 module.exports = (db) => {
+
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
       .then(data => {
@@ -21,5 +23,59 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/profile", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
+
 };
+
+//GET route to the User Id
+router.get("/:id", (req, res) => {
+  const userId = req.params,id;
+  getFavouriteMaps(userId)
+  .then(maps => res.json(maps))
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  });
+})
+
+//GET Route to user's favourites
+
+router.get(":/id/favourites", (req, res) => {
+  const userId = req.params.id;
+  getFavouriteMaps(userId)
+    .then(maps => res.json(maps))
+    .catch(err => {
+      res
+        .status(500)
+        .json({error: err.message});
+  });
+});
+
+router.post("/:id/favourites", (req, res) => {
+  const userId = req.params.id;
+  const mapId = req.body.mapId
+  addFavouriteMap(userId, mapId)
+    .then(() => {
+      return getFavouriteMaps(userId);
+    })
+    .then(result => res.json(result))
+    .catch(err => {
+      res.status(500)
+        .json({error: err.message});
+    });
+});
