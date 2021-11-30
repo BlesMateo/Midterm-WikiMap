7fa767242ded1d1e3c7330fd3f1723882dd217ff
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-30 12:46:59 -0500

    Modified favourites test database

--- db/seeds/favourites.sql
+++ db/seeds/favourites.sql
@@ -1,7 +1,5 @@
 INSERT INTO favourites (user_id, map_id)
      VALUES (1, 2)
           , (2, 3)
-          , (3, 4)
-          , (4, 5)
-          , (5, 6)
-          , (6, 7);
+          , (3, 2)
+          ;
