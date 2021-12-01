d18b4e4311c5c25f094a96e32cbdb62db53922a0
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-12-01 00:54:47 +0000

    Pull title, description and location from database for map_id

--- routes/newMap.js
+++ routes/newMap.js
@@ -5,10 +5,23 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/new/:mapId", (req, res) => {
-    let mapId = req.params.mapId
-    res.render("displayMap");
+    const templateVars =  { mapId: req.params.mapId }
 
+    return db
+    .query(`SELECT * FROM maps WHERE id = $1`, [req.params.mapId])
+    .then(result => {
+      if (result.rows[0]) {
+        let templateVars = { title: result.rows[0].title, description: result.rows[0].description, location: result.rows[0].location }
+        res.render("displayMap", templateVars )
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(error => console.log(error.message));
   });
+
+
 
   return router;
 };
