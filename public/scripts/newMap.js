2c5b37de98481201ecde1282cef985aa497e4e3c
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-12-01 01:53:16 +0000

    Refactored

--- public/scripts/newMap.js
+++ public/scripts/newMap.js
@@ -1,26 +1,21 @@
+
+
 function initMap() {
-  var center = {lat: 34.052235, lng: -118.243683};
-  var locations = [
-    ['Philz Coffee<br>\
-    801 S Hope St A, Los Angeles, CA 90017<br>\
-   <a href="https://goo.gl/maps/L8ETMBt7cRA2">Get Directions</a>',   34.046438, -118.259653],
-    ['Philz Coffee<br>\
-    525 Santa Monica Blvd, Santa Monica, CA 90401<br>\
-   <a href="https://goo.gl/maps/PY1abQhuW9C2">Get Directions</a>', 34.017951, -118.493567],
-    ['Philz Coffee<br>\
-    146 South Lake Avenue #106, At Shoppers Lane, Pasadena, CA 91101<br>\
-    <a href="https://goo.gl/maps/eUmyNuMyYNN2">Get Directions</a>', 34.143073, -118.132040],
-    ['Philz Coffee<br>\
-    21016 Pacific Coast Hwy, Huntington Beach, CA 92648<br>\
-    <a href="https://goo.gl/maps/Cp2TZoeGCXw">Get Directions</a>', 33.655199, -117.998640],
-    ['Philz Coffee<br>\
-    252 S Brand Blvd, Glendale, CA 91204<br>\
-   <a href="https://goo.gl/maps/WDr2ef3ccVz">Get Directions</a>', 34.142823, -118.254569]
-  ];
-var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    center: center
-  });
+    // Initialize variables
+    bounds = new google.maps.LatLngBounds();
+    infoWindow = new google.maps.InfoWindow;
+    currentInfoWindow = infoWindow;
+    /* TODO: Step 4A3: Add a generic sidebar */
+    infoPane = document.getElementById('panel');
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15
+    });
+
+    infoWindow.open(map);
+
+    geoFunc("", document.getElementById('location').innerText);
+
+//Markers -- needs to be updated to grab markers
 var infowindow =  new google.maps.InfoWindow({});
 var marker, count;
 for (count = 0; count < locations.length; count++) {
@@ -36,4 +31,24 @@ google.maps.event.addListener(marker, 'click', (function (marker, count) {
       }
     })(marker, count));
   }
+}
+//Geo Function to center in on a location
+function geoFunc(keyword, location) {
+  geocoder =  new google.maps.Geocoder();
+    // const urlSearchParams = new URLSearchParams(window.location.search);
+    // const params = Object.fromEntries(urlSearchParams.entries());
+    // console.log(params);
+    geocoder
+      .geocode({ 'address': location })
+      .then((result) => {
+        const { results } = result;
+        pos = results[0].geometry.location
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Searching near here.');
+        map.setCenter(pos);
+
+        /* TODO: Step 3B2, Call the Places Nearby Search */
+        // Call Places Nearby Search on user's location
+        getNearbyPlaces(pos, keyword);
+      })
 }
