772c1142f16638dd2cec3fe6f482b6ee670b152e
Walid <walid_douri@hotmail.com>
2021-12-03 20:48:06 -0500

    Back Button & images for GitHub Display

--- db/schema/deadspace.sql
+++ db/schema/deadspace.sql
@@ -48,3 +48,39 @@ CREATE TABLE favourites (
 );
 
 
+INSERT INTO users (name, email, password, description)
+VALUES('James', 'JC@gmail.com', 'Ilovepizza', 'description');
+INSERT INTO users (name, email, password, description)
+VALUES('Vega', 'vega@gmail.com', 'password', 'description');
+INSERT INTO users (name, email, password, description)
+VALUES('John', 'jpan@gmail.com', 'password', 'description');
+INSERT INTO users (name, email, password, description)
+VALUES('Shane', 'shanei@gmail.com', 'password', 'description');
+
+INSERT INTO maps (title, description, location, user_id) VALUES ('Best Picnic Grounds', 'Description','Toronto', 1);
+INSERT INTO maps (title, description, location, user_id) VALUES ('Dog Parks', 'Description','Toronto', 1);
+INSERT INTO maps (title, description, location, user_id) VALUES ('Locations of Movie Scenes', 'Description','Vancouver', 3);
+INSERT INTO maps (title, description, location, user_id) VALUES ('Best Places to Eat Around Town', 'Description','Toronto', 1);
+INSERT INTO maps (title, description, location, user_id) VALUES ('Must Go To K-BBQ Spots', 'Description','Vancouver', 4);
+INSERT INTO maps (title, description, location, user_id) VALUES ('Best Hiking Spots in Toronto!!', 'Description','Toronto', 2);
+
+
+INSERT INTO marker (title, description, map_id, lng, lat, place_id) VALUES ('Yes Resto', 'Description', 3, -79.347015, 43.651070, 'placeholderID');
+INSERT INTO marker (title, description, map_id, lng, lat, place_id) VALUES ('Great Lakes Waterfront Trail', 'Description', 2, -79.15169729807835, 4-79.15169729807835, 'placeholderID');
+INSERT INTO marker (title, description, map_id, lng, lat, place_id) VALUES ('Evergreen Brick Works', 'Description', 1, -79.3645369087173, -79.3645369087173, 'placeholderID');
+INSERT INTO marker (title, description, map_id, lng, lat, place_id) VALUES ('No Resto', 'Description', 2, -79.347015, 43.651070, 'placeholderID');
+INSERT INTO marker (title, description, map_id, lng, lat, place_id) VALUES ('Burgers n Fries Forever', 'Description', 1, -79.39018483131264, 43.649458679470904, 'placeholderID');
+
+
+INSERT INTO contributions(user_id, map_id) VALUES (1, 2);
+INSERT INTO contributions(user_id, map_id) VALUES (2, 3);
+INSERT INTO contributions(user_id, map_id) VALUES (3, 2);
+INSERT INTO contributions(user_id, map_id) VALUES (1, 3);
+INSERT INTO contributions(user_id, map_id) VALUES (2, 3);
+INSERT INTO contributions(user_id, map_id) VALUES (1, 1);
+
+INSERT INTO favourites (user_id, map_id) VALUES (2, 3);
+INSERT INTO favourites (user_id, map_id) VALUES (3, 2);
+INSERT INTO favourites (user_id, map_id) VALUES (1, 3);
+INSERT INTO favourites (user_id, map_id) VALUES (3, 2);
+INSERT INTO favourites (user_id, map_id) VALUES (1, 3);
